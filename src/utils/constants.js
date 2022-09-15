1c05c7fb3503cef699fc64cf580cb66babcc3079
Stanley Hayford <hayfordstanley@gmail.com>
2022-09-15 10:42:01 +0000

    Change server to production server

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,4 @@
-const SERVER_BASE_URL = 'http://localhost:5000';
-// const SERVER_BASE_URL = 'https://screentime-api.herokuapp.com';
+// const SERVER_BASE_URL = 'http://localhost:5000';
+const SERVER_BASE_URL = 'https://screentime-api.herokuapp.com';
 
 export const CONSTANTS = {SERVER_BASE_URL};
